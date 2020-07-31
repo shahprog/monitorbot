068f87a8e7757a74c8831ba5432487aa2de4dd47
shahprog <mdshahriyaralam552@gmail.com>
2020-07-31 19:10:24 +0600

    Added settz command

--- cogs/config.py
+++ cogs/config.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import pytz
 
 
 class Config(commands.Cog):
@@ -28,6 +29,23 @@ class Config(commands.Cog):
             ctx.guild.id,
         )
         await ctx.send(f"Prefix changed to {new_prefix}.")
+
+    @commands.command()
+    async def settz(self, ctx, timezone: str):
+        """Set timezone to get exact status info"""
+        try:
+            pytz.timezone(timezone)
+        except pytz.UnknownTimeZoneError:
+            return await ctx.send("Invalid timezone. List of valid timezone <https://en.wikipedia.org/wiki/List_of_tz_database_time_zones>")
+
+        data = await self.bot.db.fetchrow("SELECT * FROM user_tz WHERE userid = $1", ctx.author.id)
+
+        if data:
+            await self.bot.db.execute("UPDATE user_tz SET tz=$1 WHERE id=$2", timezone, data['id'])
+        else:
+            await self.bot.db.execute("INSERT INTO user_tz(userid, tz) VALUES($1, $2)", ctx.author.id, timezone)
+
+        await ctx.send('Timezone updated successfully.')
 
 
 def setup(bot):
